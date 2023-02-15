0e54888e8d3d6963da03b897fb325dacce508084
yany2 <46777856+yany2@users.noreply.github.com>
2023-02-15 11:35:34 +0100

    add glad submodule and CMake library

--- 3D-tetris/src/3D-tetris.cpp
+++ 3D-tetris/src/3D-tetris.cpp
@@ -2,11 +2,26 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#include <glad/gl.h>
+#include <glfw3.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "hello glfw & glad2", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	gladLoadGL(glfwGetProcAddress);
+	glViewport(0, 0, 640, 480);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	for (int i = 0; i < 600; i++) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
 	return 0;
 }
