614de85fa16e4579aaf994163d8a72e91074d639
yany2 <46777856+yany2@users.noreply.github.com>
2023-02-15 09:25:01 +0100

    add libraries to CMake

--- 3D-tetris/3D-tetris.h
+++
@@ -1,8 +0,0 @@
-﻿// 3D-tetris.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
---
+++ 3D-tetris/src/3D-tetris.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
